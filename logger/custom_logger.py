3a5e89f6f8a38ad23610e1da4f4ecc422a29f76d
Mohammed Munavar <bs.munavar@gmail.com>
2025-08-04 07:03:12 +0530

    Updated logger file with strctlog

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,23 +1,50 @@
 import logging
 import os
 from datetime import datetime
+import structlog
 
 class CustomLogger:
     def __init__(self, log_dir="logs"):
-        logs_dir = os.path.join(os.getcwd(),"logs")
-        os.makedirs(logs_dir, exist_ok=True)
+        self.logs_dir = os.path.join(os.getcwd(),"logs")
+        os.makedirs(self.logs_dir, exist_ok=True)
 
         log_file = f"{datetime.now().strftime('%m_%d_%y_%Y_%H_%M_%S')}.log"
-        log_file_path = os.path.join(logs_dir, log_file)
+        self.log_file_path = os.path.join(self.logs_dir, log_file)
+
+
+    def get_logger(self,name=__file__):
+        logger_name = os.path.basename(name)
+
+        file_handler = logging.FileHandler(self.log_file_path)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter("%(message)s"))
+
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
 
         logging.basicConfig(
-            filename = log_file_path,
-            format = "[ %(asctime)s ] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
-            level = logging.INFO
-)
-    def get_logger(self,name=__file__):
-        return logging.getLogger(os.path.basename(name))
+            level=logging.INFO,
+            format="%(message)s",
+            handlers=[console_handler, file_handler]
+        )
+
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.JSONRenderer()
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use=True
+        )
+        return structlog.get_logger(logger_name)
+
+
+
 if __name__=="__main__":
-    logger = CustomLogger()
-    logger = logger.get_logger(__file__)
-    logger.info("Custom logger is initialized")
+    logger = CustomLogger().get_logger(__file__)
+    logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+    logger.error("Failed to process PDF", error="File is not found", user_id=123)
