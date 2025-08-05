1e1bb9fef5bdffa005477572826ae66266a8b6d4
Mohammed Munavar <bs.munavar@gmail.com>
2025-08-05 17:23:31 +0530

    Code updated for document analysis

--- src/document_analyzer/analysis.py
+++ src/document_analyzer/analysis.py
@@ -23,5 +23,17 @@ class DocumentAnalyzer:
             self.log.error(f"Error initializing DocumentAnalyzer: {e}")
             raise DocumentPortalExeption("Error in DocumentAnalyzer initialization",sys)
 
-    def analyze_document(self):
-        pass
+    def analyze_document(self, document_text:str)->dict:
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            self.log.info("Meta data analysis chain initialized")
+            
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+            self.logo.info("MetaData extraction successful", keys=list(response.keys()))
+            return response
+        except Exception as e:
+            self.log.error("Metadata analysis failed", error=str(e))
+            raise DocumentPortalExeption("Metadata extraction failed") from e
