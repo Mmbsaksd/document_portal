8c61c022cee4bca51701ee6ca0bc39f86857bc68
Mohammed Munavar <bs.munavar@gmail.com>
2025-08-05 06:51:21 +0530

    Document analyser created

---
+++ src/document_analyzer/analysis.py
@@ -0,0 +1,15 @@
+import os
+from utils.model_loader import Model_loader
+from logger.custom_logger import CustomLogger
+from exeption.custom_exeption import DocumentPortalExeption
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+
+class DocumentAnalyzer:
+    def __init__(self):
+        pass
+
+    def analyze_metadata(self):
+        pass
---
+++ src/document_analyzer/data_ingestion.py
@@ -0,0 +1,16 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exeption.custom_exeption import DocumentPortalExeption
+
+class DocumentHandler:
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
