a6f5c91d622f464088f77f797b8c5a7730b9879e
Mohammed Munavar <bs.munavar@gmail.com>
2025-08-15 06:44:16 +0530

    code updated

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ def test_conversional_rag_pdf(pdf_path:str, question:str):
 
 if __name__=="__main__":
     pdf_path = r"C:\\Users\\bsmun\\document_portal\\data\\single_document_chat\\NIPS-2017-attention-is-all-you-need-Paper.pdf"
-    question = "What is the main topic of the document?"
+    question = "What is the attention mechanism in simple term?"
 
     #Run the test
     test_conversional_rag_pdf(pdf_path,question)
