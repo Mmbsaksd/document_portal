ff8d404280f390fd38e1a7ec794d3788cb77fc86
Mohammed Munavar <bs.munavar@gmail.com>
2025-08-27 10:36:36 +0530

    Working fine with docar image

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,12 @@
+import os
 import yaml
-def load_config(config_path: str="config\config.yaml")->dict:
-    with open(config_path,"r") as file:
-        config = yaml.safe_load(file)
-    return config
 
-load_config("config/config.yaml")
+def load_config(config_path: str = None) -> dict:
+    # Always resolve path relative to /app (works in Docker and locally)
+    base_dir = os.path.dirname(os.path.dirname(__file__))  # -> /app
+    default_path = os.path.join(base_dir, "config", "config.yaml")
+
+    path = config_path or default_path
+
+    with open(path, "r") as file:
+        return yaml.safe_load(file)
